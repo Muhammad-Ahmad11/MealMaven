849f1d2574aa144809ed9b73384783b7815ad4d5
Wasi Hyder <106431305+wasihyderr@users.noreply.github.com>
2023-04-13 19:58:08 +0500

    Basic UI Changes + Wrapper Class Added
    -- Added a Wrapper Class for the Admin and User Button on the home page.
    
    -- Connected the Wrapper Buttons with their relevant pages through NavLink
    
    -- Cleaned the User Signup Page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,34 +1,42 @@
 import React from 'react'
 import food from '../images/Home.jpg'
 import Navbar from './Navbar'
+import { NavLink } from 'react-router-dom';
 
 const Home = () => {
   return (
     <>
-        {/* <div className="home-page">
-            <div className="col-md-4">
-                <p className ="pt-5">Welcome</p>
-                <h1>We are the MealMaven</h1>
+      <Navbar />
+      <div className="home-page">
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-md-6">
+              <h1 className="display-4">We are the MealMaven</h1>
+              <p className="lead mb-5">Find the perfect meal for any occasion</p>
             </div>
-        </div> */}
-        <Navbar/>
-        <div className="home-page">
-      <div className="container">
-        <div className="row align-items-center">
-          <div className="col-md-6">
-            <h1 className="display-4">We are the MealMaven</h1>
-            <p className="lead mb-5">Find the perfect meal for any occasion</p>
-          </div>
-          <div className="col-md-6">
-          <img
-              src={food}
-              alt="Delicious Food"
-            />
+            <div className="col-md-6">
+              <img src={food} alt="Delicious Food" />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <ul className="wrapper">
+        <NavLink to="/Admin">
+        <li className="icon admin">
+        <span className="tooltip">Admin</span>
+        <span><i className="fab fa-admin-f"></i></span>
+        </li>
+        </NavLink>
+        <NavLink to="/UserSignup">
+        <li className="icon user">
+        <span className="tooltip">User</span>
+        <span><i className="fab fa-user"></i></span>
+        </li>
+        </NavLink>
+      </ul>
+
     </>
   )
 }
+
 export default Home
